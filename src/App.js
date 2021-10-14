27b9d6395e4ea40e00e57b5c6c19a4a4f451bdf9
William Martin <wjkmartin@gmail.com>
2021-10-14 11:24:20 -0700

    Setup app skeleton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom"
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <Router>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/search">
+        <Search />
+      </Route>
+    </Switch>
+    </Router>
   );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  )
+}
+
+function Search() {
+  return (
+    <div>
+      <h2>Search</h2>
+    </div>
+  )
 }
 
 export default App;
