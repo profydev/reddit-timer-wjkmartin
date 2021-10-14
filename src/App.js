1d536f8e991420f57c32dfc39522e97a0fd18d42
William Martin <wjkmartin@gmail.com>
2021-10-14 11:35:54 -0700

    Fix linting

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route exact path="/search">
-        <Search />
-      </Route>
-    </Switch>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/search">
+          <Search />
+        </Route>
+      </Switch>
     </Router>
   );
 }
@@ -25,7 +21,7 @@ function Home() {
     <div>
       <h2>Home</h2>
     </div>
-  )
+  );
 }
 
 function Search() {
@@ -33,7 +29,7 @@ function Search() {
     <div>
       <h2>Search</h2>
     </div>
-  )
+  );
 }
 
 export default App;
