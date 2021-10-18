d352b192e94f527cef50efab5f6aeaf8bb67e29d
William Martin <wjkmartin@gmail.com>
2021-10-18 09:41:34 -0700

    Create app skeleton (#1)
    * Setup app skeleton
    
    * Fix linting
    
    * Change app title and meta description

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/search">
+          <Search />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
     <div>
-      App Placeholder
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function Search() {
+  return (
+    <div>
+      <h2>Search</h2>
     </div>
   );
 }
