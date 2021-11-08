4f3704d95063769751e4e27da332fa94042bfaa4
William Martin <wjkmartin@gmail.com>
2021-11-08 14:17:17 -0800

    Hero section (#4)
    * Create footer
    
    * Create hero section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import styles from './Home.module.css';
 
 const Home = () => (
   <div className={styles.Home}>
-    <h2>Home</h2>
+    <h1 className={styles.Hero__title}>No reactions to your reddit posts?</h1>
+    <p className={styles.Hero__subTitle}>
+      Great timing, great results! Find the best time to post on your subreddit.
+    </p>
+    <Link className={styles.Hero__linkButton} to="/search#javascript">
+      SHOW ME THE BEST TIME
+    </Link>
+    <p className={styles.Hero__subreddit}>r/javascript</p>
+    <Link to="/search#javascript">
+      <img
+        className={styles.Hero__image}
+        alt="A heatmap displaying when the best time is to post of r/javascript"
+        src="/table.png"
+      />
+    </Link>
   </div>
 );
 
