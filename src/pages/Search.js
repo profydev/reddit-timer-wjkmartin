f8df0bd2f91daa68e58830e481a249fb521cebab
William Martin <wjkmartin@gmail.com>
2021-11-12 07:30:48 -0800

    Fix to make test pass

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,32 +1,42 @@
-import React from 'react';
-import { useParams, Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
 
 import styles from './Search.module.css';
 
 const Search = () => {
-  const [subreddit, setSubreddit] = React.useState(useParams().subreddit);
+  const history = useHistory();
+  const subredditInitial = useParams().subreddit;
+  const [subReddit, setSubreddit] = React.useState(subredditInitial);
+  function handleSubmit(event) {
+    event.preventDefault();
+    history.push(`/search/${subReddit}`);
+  }
+
+  const onChange = (event) => {
+    setSubreddit(event.target.value);
+  };
+  useEffect(() => {
+    setSubreddit(subredditInitial);
+  }, [subredditInitial]);
+
   return (
     <div className={styles.Search}>
       <h1 className={styles.Search__title}>
         Find the best time for a subreddit
       </h1>
-      <div
-        className={styles.Search__inputArea}
-      >
+      <form onSubmit={handleSubmit} className={styles.Search__inputArea}>
         <p className={styles.Search__preLabel}>r / </p>
         <input
           className={styles.Search__inputBox}
           type="text"
-          onChange={(evt) => setSubreddit(evt.target.value)}
-          defaultValue={subreddit}
+          name="subreddit"
+          onChange={onChange}
+          value={subReddit}
         />
-        <Link
-          className={styles.Search__submitButton}
-          to={`/search/${subreddit}`}
-        >
-          SEARCH
-        </Link>
-      </div>
+        <button className={styles.Search__submitButton} type="submit">
+          Search
+        </button>
+      </form>
     </div>
   );
 };
