c679ffd175a97f81108bdd787e168c5d35e232a5
William Martin <wjkmartin@gmail.com>
2021-11-13 07:51:27 -0800

    Fix fetching

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,3 +1,4 @@
+/* eslint-disable comma-dangle */
 import React, { useState, useEffect } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 
@@ -5,34 +6,63 @@ import styles from './Search.module.css';
 
 const Search = () => {
   const history = useHistory();
-  const subredditInitial = useParams().subreddit;
-
+  const [subredditInitial, setSubredditInitial] = useState(
+    useParams().subreddit
+  );
   const [subReddit, setSubreddit] = useState(subredditInitial);
-  const [isLocaded, setIsLoaded] = useState(false);
+  let inputValue = subredditInitial;
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [subredditInsufData, setSubredditInsufData] = useState(false);
+  const [isInvalidSubreddit, setIsInvalidSubreddit] = useState(false);
   function handleSubmit(event) {
     event.preventDefault();
     history.push(`/search/${subReddit}`);
+    setSubredditInitial(inputValue);
+    setSubreddit(inputValue);
   }
 
   const onChange = (event) => {
-    setSubreddit(event.target.value);
+    inputValue = event.target.value;
   };
-  useEffect(() => {
-    setSubreddit(subredditInitial);
 
-    fetch(`https://www.reddit.com/r/${subReddit}/top.json?t=year&limit=500`)
-      .then((response) => response.json())
-      .then(
-        (data) => {
-          setIsLoaded(true);
-          console.log(data);
+  useEffect(() => {
+    async function fetchPosts({ previousPosts = [], after }, _subReddit) {
+      if (previousPosts.length >= 500) {
+        return previousPosts;
+      }
+
+      const response = await fetch(
+        after
+          ? `https://www.reddit.com/r/${_subReddit}/top.json?t=year&limit=100&after=${after}`
+          : `https://www.reddit.com/r/${_subReddit}/top.json?t=year&limit=100`
+      ).then((res) => res.json());
+      if (response.data.children.length < 100) {
+        setSubredditInsufData(true);
+        return null;
+      }
+
+      return fetchPosts(
+        {
+          previousPosts: previousPosts.concat(response.data.children),
+          after: response.data.after,
         },
-        (error) => {
-          setIsLoaded(true);
-          console.log(error);
-        },
+        _subReddit
       );
-  }, [subredditInitial, subReddit]);
+    }
+
+    const posts = fetchPosts({}, subReddit);
+
+    posts
+      .catch(() => {
+        if (!subredditInsufData) {
+          setSubredditInsufData(false);
+          setIsInvalidSubreddit(true);
+        }
+      })
+      .finally(() => {
+        setIsLoaded(true);
+      });
+  }, [subReddit, subredditInsufData, isInvalidSubreddit]);
 
   return (
     <div className={styles.Search}>
@@ -46,14 +76,13 @@ const Search = () => {
           type="text"
           name="subreddit"
           onChange={onChange}
-          value={subReddit}
+          defaultValue={subredditInitial}
         />
         <button className={styles.Search__submitButton} type="submit">
           Search
         </button>
       </form>
-
-      {isLocaded ? (
+      {isLoaded ? (
         ''
       ) : (
         <img
